b301997729602fd8a60956f2b6607e8b0e8547aa
Yusuf6256 <78488257+Yusuf6256@users.noreply.github.com>
2022-02-25 19:52:20 +0530

    Add files via upload

---
+++ cod2.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+import plotly.graph_objects as pg
+
+df = pd.read_csv("data.csv")
+print(df.groupby("level")["attempt"].mean())
+
+fig = pg.Figure(pg.Bar(
+    x=df.groupby("level")["attempt"].mean(),
+    y=["level 1","level 2","level 3","level 4"],
+    orientation='h'
+))
+fig.show()
